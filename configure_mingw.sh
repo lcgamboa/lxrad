2eba2f00a1e523cba23ad714058f73e60e5dc38e
Luis Claudio Gamboa Lopes <lcgamboa@yahoo.com>
2015-11-11 18:34:31 -0200

    Cross compiler configure updated

--- configure_mingw.sh
+++ configure_mingw.sh
@@ -4,6 +4,5 @@ RANLIB=x86_64-w64-mingw32-ranlib
 DLLTOOL=x86_64-w64-mingw32-dlltool
 LD=x86_64-w64-mingw32-ld
 NM=x86_64-w64-mingw32-nm
-CXXFLAGS= -D_WIN_
 ./configure --host=x86_64-w64-mingw32 --with-mingw --with-wx-config=x86_64-w64-mingw32-msw-unicode-3.0  --prefix=/usr/x86_64-w64-mingw32/
 cp lib/Makefile.cross lib/Makefile
