90403b08fd53bcbb66adb19e1f633172c331cbc8
lcgamboa <lcgamboa@yahoo.com>
2021-07-24 15:47:09 -0300

    makefiles updated

--- make_deps.sh
+++ make_deps.sh
@@ -3,8 +3,8 @@ cd ..
 git clone --depth=1 https://github.com/sammycage/lunasvg.git
 cd lunasvg
 git pull --no-rebase
-export CXXFLAGS=-fPIC
-export CFLAGS=-fPIC
+export CXXFLAGS="-fPIC -flto"
+export CFLAGS="-fPIC -flto"
 rm -rf build
 mkdir build
 cd build
