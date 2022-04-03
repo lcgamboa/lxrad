5c3a168d71e969f500d08a46c5ab72780f6b3261
lcgamboa <lcgamboa@yahoo.com>
2022-04-03 18:51:17 -0300

    define luna version

--- make_deps.sh
+++ make_deps.sh
@@ -2,7 +2,8 @@
 cd ..
 git clone --depth=1 https://github.com/sammycage/lunasvg.git
 cd lunasvg
-git pull --no-rebase
+#git pull --no-rebase
+git checkout 7f82e941270aad040922798c210ce0e2c1eb58aa
 export CXXFLAGS="-fPIC -flto"
 export CFLAGS="-fPIC -flto"
 rm -rf build
