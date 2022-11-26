78ac2f1e035ef921f79b86b31aedf5f540eee3f6
lcgamboa <lcgamboa@yahoo.com>
2022-11-26 16:47:30 -0300

    disable lto

--- make_deps.sh
+++ make_deps.sh
@@ -4,8 +4,8 @@ git clone --depth=1 https://github.com/sammycage/lunasvg.git
 cd lunasvg
 #git pull --no-rebase
 git checkout 7f82e941270aad040922798c210ce0e2c1eb58aa
-export CXXFLAGS="-fPIC -flto"
-export CFLAGS="-fPIC -flto"
+export CXXFLAGS="-fPIC"
+export CFLAGS="-fPIC"
 rm -rf build
 mkdir build
 cd build
